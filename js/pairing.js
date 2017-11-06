7defd6a39aef154f2da548c859eb9f6c5ff272d1
Rod Reyes <roderick.reyes@gmail.com>
2017-11-06 13:19:41 +0100

    feat: add pairing algorithm

---
+++ js/pairing.js
@@ -0,0 +1,30 @@
+const pairingProblem = (num1, num2) => {
+  const res = []
+  const pairs = []
+  let oddPair;
+  for (let i = 1; i <= num1; i++) {
+    pairs.push(i)
+  }
+  if (num1 % 2 === 1) {
+    oddPair = pairs.pop()
+    num1 -=1
+  }
+  for (let j = 0; j < num2 - 1; j++) {
+    res[j] = []
+    for (let k = 0; k < num1/2; k++) {
+      res[j].push([pairs[k], pairs[num1 - 1 - k]]);
+    }
+    let l = Math.round(Math.random() * Math.floor(num1/2 - 1))
+    if (oddPair !== undefined) {
+      res[j][l].push(oddPair)
+      const thirdPerson = res[j][l].shift()
+      let m = Math.round(Math.random() * Math.floor(num1/2 - 1))
+      while (m === l) {
+        m = Math.round(Math.random() * Math.floor(num1/2 - 1))
+      }
+      res[j][m].push(thirdPerson)
+    }
+    pairs.splice(1, 0, pairs.pop());
+  }
+  return res;
+}
