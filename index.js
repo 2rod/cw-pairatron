7da45b86722c8958c73a23095ff14cff1286b53d
Rod Reyes <roderick.reyes@gmail.com>
2017-11-08 10:22:34 +0100

    refactor: setup port detection

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const app = new Koa();
 
 app.use(serve('src'));
 
-app.listen(3000, () => {
-  console.log('listening on port 3000...');
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}...`);
 });
