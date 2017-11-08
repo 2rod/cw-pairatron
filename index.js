d9f7034f2b1bdae17677c615f2b0796229266dc4
Rod Reyes <roderick.reyes@gmail.com>
2017-11-08 10:21:09 +0100

    feat: add koa-static for serving static files

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+const serve = require('koa-static');
 const Koa = require('koa');
 const app = new Koa();
+
+app.use(serve('src'));
 
 app.listen(3000, () => {
   console.log('listening on port 3000...');
