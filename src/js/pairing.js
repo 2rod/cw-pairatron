3874ced5dc84b88682a5bc00f8661521bc9441f9
Anna Collins <annacollins85@gmail.com>
2017-11-10 14:29:59 +0100

    function for inserting odd student done. need to change pairing function

--- src/js/pairing.js
+++ src/js/pairing.js
@@ -1,54 +1,65 @@
-const pairingProblem = (num1, num2) => {
-  const pairs = []
-  let oddPair;
-  for (let i = 1; i <= num1; i++) {
-    pairs.push(i)
+const pairingProblem = (students, rounds) => {
+  const studentsArr = []
+  let oddStudent;
+  for (let i = 1; i <= students; i++) {
+    studentsArr.push(i)
   }
-  if (num1 % 2 === 1) {
-    if (num1 == 3) { // handle special case for 3
-      return generateTrioCase(pairs, num2);
-    } else oddPair = pairs.pop()
-      num1 -=1
+  if (students % 2 === 1) {
+    if (students == 3) { // handle special case for 3
+      return generateTrioCase(studentsArr, rounds);
+    } else oddStudent = studentsArr.pop()
+      students -=1
+      const roundsOfPairs = generatePairs(studentsArr, rounds)
+      return handleOddStudent(roundsOfPairs, oddStudent);
   }
-  return generatePairs(pairs, num2, oddPair);
+  return generatePairs(studentsArr, rounds);
 }
 
-const generatePairs = (numArr, numRounds, extraNum) => {
-  const res = [];
-  const halfLen = numArr.length/2;
-  for (let j = 0; j < numRounds; j++) {
-    res[j] = [];
-    for (let k = 0; k < halfLen; k++) {
-      res[j].push([numArr[k], numArr[numArr.length - 1 - k]]);
+const generatePairs = (studentsArr, rounds) => {
+  const pairs = [];
+  const noOfPairs = studentsArr.length/2;
+  for (let j = 0; j < rounds; j++) {
+    pairs[j] = [];
+    for (let k = 0; k < noOfPairs; k++) {
+      pairs[j].push([studentsArr[k], studentsArr[studentsArr.length - 1 - k]]);
     }
-    let l = getRandomInt(halfLen);
-    if (extraNum !== undefined) {
-      res[j][l].push(extraNum);
-      const extraNum2 = res[j][l].shift();
-      let m = getRandomInt(halfLen);
-      m = getRandomInt(halfLen);
-      res[j][m].push(extraNum2);
-    }
-    numArr.splice(1, 0, numArr.pop());
+    studentsArr.splice(1, 0, studentsArr.pop());
   }
-  return res;
+  return pairs;
 }
 
-const generateTrioCase = (numArr, numRounds) => {
+const handleOddStudent = (roundsOfPairs, oddStudent) => {
+  let calcTrio = 0;
+  const totalStudents = oddStudent;
+  roundsOfPairs.forEach(setOfPairs => {
+    setOfPairs.forEach(pair => {
+      pair.forEach((student, i) => {
+        if (student >= oddStudent) {
+          student++
+          pair[i] = student;
+        }
+      })
+    })
+    const trioPair = calcTrio % roundsOfPairs.length
+    setOfPairs[trioPair].push(oddStudent)
+    calcTrio++
+    if (oddStudent > 1) oddStudent --
+    else oddStudent = totalStudents
+  })
+  return roundsOfPairs;
+}
+
+const generateTrioCase = (numArr, rounds) => {
   let res = [];
   let round = [];
   let single;
-  while (numRounds) {
+  while (rounds) {
     single = numArr.shift();
     round = [];
     round.push([single], [...numArr]);
     res.push(round);
     numArr.push(single);
-    numRounds--;
+    rounds--;
   }
   return res;
-}
-
-const getRandomInt = (max) => {
-  return Math.round(Math.random() * Math.floor(max - 1))
 }
